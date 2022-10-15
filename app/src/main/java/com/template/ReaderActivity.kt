864e1b19b08c3b7122beba9ada40ce06350602f3
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2022-10-15 18:49:23 +0300

    Реализована обработка нажатия на кнопки Вперёд и Назад, а также сохранение номера страницы в SharedPreferences и извлечение его оттуда.

--- app/src/main/java/com/template/ReaderActivity.kt
+++ app/src/main/java/com/template/ReaderActivity.kt
@@ -4,20 +4,30 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.navigation.NavigationView
 import com.template.databinding.ActivityReaderBinding
+import java.io.IOException
+import kotlin.collections.ArrayList
+
 
 class ReaderActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityReaderBinding
+
+    private val pages: ArrayList<String> = ArrayList()
+    private var currentPage = 0
+
+    private lateinit var backButton: MaterialButton
+    private lateinit var nextButton: MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +38,49 @@ class ReaderActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarReader.toolbar)
         initDrawer(binding.appBarReader.toolbar)
 
-        initWebView()
+        listAssetFiles("pages")
+
+        backButton = binding.appBarReader.contentReader.backButton
+        backButton.setOnClickListener { back() }
+
+        nextButton = binding.appBarReader.contentReader.nextButton
+        nextButton.setOnClickListener { next() }
+
+        loadCurrentPage()
+
+        enableButtons()
+
+        showPageInWebView()
+    }
+
+    /**
+     * Устанавливает доступность кнопок Вперёд и Назад в зависимости от того, какая страница чейчас открыта
+     */
+    private fun enableButtons() {
+        backButton.isEnabled = currentPage != 0
+        nextButton.isEnabled = currentPage != pages.size - 1
+    }
+
+    /**
+     * Достаёт файлы из папки assets и добавляет их в ArrayList pages
+     */
+    private fun listAssetFiles(path: String): Boolean {
+        val list: Array<String>?
+        try {
+            list = assets.list(path)
+            if (list!!.isNotEmpty()) {
+                // Это папка
+                for (file in list) {
+                    if (!listAssetFiles("$path/$file")) return false else {
+                        // Это файл
+                        pages.add(file)
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            return false
+        }
+        return true
     }
 
     private fun initDrawer(toolbar: Toolbar) {
@@ -92,7 +144,46 @@ class ReaderActivity : AppCompatActivity() {
     /**
      * Отображение html-документов в WebView
      */
-    private fun initWebView() {
-        binding.appBarReader.contentReader.webView.loadUrl("file:///android_asset/1.html")
+    private fun showPageInWebView() {
+        val name = pages.get(currentPage)
+        binding.appBarReader.contentReader.webView.loadUrl("file:///android_asset/pages/$name")
+    }
+
+    /**
+     * Отображает следующую страницу
+     */
+    private fun next() {
+        currentPage++
+        enableButtons()
+        showPageInWebView()
+        saveCurrentPage()
+    }
+
+    /**
+     * Отображает предыдущую страницу
+     */
+    private fun back() {
+        currentPage--
+        enableButtons()
+        showPageInWebView()
+        saveCurrentPage()
+    }
+
+    /**
+     * Сохраняет номер текущей страницы в SharedPreferences
+     */
+    private fun saveCurrentPage() {
+        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt("page", currentPage)
+        editor.apply()
+    }
+
+    /**
+     * Загружает номер текущей страницы из SharedPreferences
+     */
+    private fun loadCurrentPage() {
+        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        currentPage = sharedPreferences.getInt("page", 0)
     }
 }
